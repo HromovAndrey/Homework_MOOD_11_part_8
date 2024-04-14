67f44877414ccd6e9ef5d75eb8d8bf1ea010a448
user <andrey.gromov.36912@gmail.com>
2024-04-14 21:09:43 +0300

    Домашня мод 11 часть 8

--- Homework-8.py
+++ Homework-8.py
@@ -1,14 +1,29 @@
-# Завдання 3
-#  Створіть метаклас, який автоматично додає певні
-# атрибути до всіх класів, що використовують його.
-
-class AutoAttributeMeta(type):
+# Завдання 4
+# Метаклас, що додає перевірку та обробку аргументів
+# __init__ у всіх класах.
+class ArgumentProcessingMeta(type):
     def __init__(cls, name, bases, attrs):
-        cls.new_attribute = "This is a new attribute added automatically"
+        original_init = getattr(cls, '__init__', None)
+
+        def new_init(self, *args, **kwargs):
+            if original_init:
+
+                processed_args = tuple(arg.upper() if isinstance(arg, str) else arg for arg in args)
+                processed_kwargs = {key: value.upper() if isinstance(value, str) else value for key, value in
+                                    kwargs.items()}
+                original_init(self, *processed_args, **processed_kwargs)
+        cls.__init__ = new_init
+
         super().__init__(name, bases, attrs)
-class MyClass(metaclass=AutoAttributeMeta):
-    pass
-print(MyClass.new_attribute)  # Виведе: This is a new attribute added automatically
+
+class MyClass(metaclass=ArgumentProcessingMeta):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+obj = MyClass("John", 30)
+print(obj.name)
+print(obj.age)
 
 
 
